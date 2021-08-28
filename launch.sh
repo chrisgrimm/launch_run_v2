407a4328c875c3cbf105c6425c0edf1fdf9c6596
Christopher Ryan Grimm <crgrimm@rldl1.eecs.umich.edu>
2021-08-28 17:30:34 -0400

    moop

--- launch.sh
+++ launch.sh
@@ -13,5 +13,6 @@ stty -echo
 printf "Password: "
 read password
 stty echo
+printf "\n"
 echo "$password" | sudo -S ./spin_up_docker.sh $1 "$password"
 echo "$password" | sudo -S docker exec $1 python3.8 -u ./launch_cluster.py "$1" "$2" "$3" "$password"
